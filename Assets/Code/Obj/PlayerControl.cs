51160de231dd5908d0c6a1f83d0c94e1c13c5409
ZJZL-20200612IB\Administrator <2281417626@qq.com>
2022-04-01 23:29:27 +0800

    null

--- Assets/Code/Obj/PlayerControl.cs
+++ Assets/Code/Obj/PlayerControl.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    public Rigidbody rig;
     // Start is called before the first frame update
+    float H = 0;
+    float V = 0;
+    private void Awake()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
     void Start()
     {
         
@@ -13,6 +20,14 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        H = Input.GetAxis("Horizontal");
+        V = Input.GetAxis("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        rig.AddForce(transform.forward*V, ForceMode.VelocityChange);
+        Vector3 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        print(dir);
+       // transform.forward = 
     }
 }
